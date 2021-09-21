052ba1d4a8f97b51afcac631a6a6976b9847a595
Rennan Oliveira <rennan_neves@live.com>
2021-09-21 14:18:46 -0300

    feat add navigation

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,36 @@
+import { GetServerSideProps } from "next"
+import { Post, PostService } from "oliveirarennan-alganews-sdk"
+import {ParsedUrlQuery} from "querystring"
+
+interface PostProps {
+  post: Post.Detailed
+}
+
+export default function PostPage(props: PostProps){
+ 
+  return <div>
+    {props.post.title}
+  </div>
+}
+
+interface Params extends ParsedUrlQuery{
+  id: string;
+}
+
+export const getServerSideProps: GetServerSideProps<PostProps, Params> = async ({params}) => {
+  
+  if(!params) return {notFound: true}
+
+  const {id} = params
+  const postId = Number(id)
+
+  if(isNaN(postId)) return {notFound: true}
+
+  const post = await PostService.getExistingPost(postId);
+
+  return {
+    props: {
+      post,
+    }
+  }
+}
